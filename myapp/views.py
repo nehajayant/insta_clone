5981a9bd71e03eb4bb8a17cb18ac3fbf95d20e4f
nehajayant <nehajayant1234@gmail.com>
2017-07-28 13:49:21 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,160 @@
+from datetime import timedelta
+
+import imgurpython
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import render, redirect
+from django.utils import timezone
+
+from forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
+from insta_clone.settings import BASE_DIR
+from models import UserModel, SessionToken, PostModel, LikeModel, CommentModel
+
+from imgurpython import ImgurClient
+from clarifai.rest import ClarifaiApp
+app = ClarifaiApp(api_key='a8bdc1c4de4240769c6a8f98027e511a')
+
+model = app.models.get('food-items-v1.0')
+# Create your views here.
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            # saving data to DB
+            user = UserModel(name=name, password=make_password(password), email=email, username=username)
+            user.save()
+            return render(request, 'success.html')
+            # return redirect('login/')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'index.html', {'form': form})
+
+
+def login_view(request):
+    response_data = {}
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = UserModel.objects.filter(username=username).first()
+
+            if user:
+                if check_password(password, user.password):
+                    token = SessionToken(user=user)
+                    token.create_token()
+                    token.save()
+                    response = redirect('feed/')
+                    response.set_cookie(key='session_token', value=token.session_token)
+                    return response
+                else:
+                    response_data['message'] = 'Incorrect Password! Please try again!'
+
+    elif request.method == 'GET':
+        form = LoginForm()
+
+    response_data['form'] = form
+    return render(request, 'login.html', response_data)
+
+
+def post_view(request):
+    user = check_validation(request)
+
+    if user:
+        if request.method == 'POST':
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                image = form.cleaned_data.get('image')
+                caption = form.cleaned_data.get('caption')
+                post = PostModel(user=user, image=image, caption=caption)
+                post.save()
+
+                path = str(BASE_DIR + post.image.url)
+
+                client = ImgurClient("f68b59fe6bad67d", "033e640bc6aea0a48d3d5c6e3bf3e61b28f4836b")
+                post.image_url = client.upload_from_path(path, anon=True)['link']
+                post.save()
+                clarifai_data = []
+                app = ClarifaiApp(api_key='fcfdca12d67a4af7b657c4117ea90128')  # Covers all scopes
+                model = app.models.get("general-v1.3")
+                result = model.predict_by_url(url=post.image_url)
+                for x in range(0, len(result['outputs'][0]['data']['concepts'])):
+                    model = result['outputs'][0]['data']['concepts'][x]['name']
+                    clarifai_data.append(model)
+                for z in range(0, len(clarifai_data)):
+                    print clarifai_data[z]
+
+
+                return redirect('/feed/')
+
+        else:
+            form = PostForm()
+        return render(request, 'post.html', {'form': form})
+    else:
+        return redirect('/login/')
+
+
+def feed_view(request):
+    user = check_validation(request)
+    if user:
+
+        posts = PostModel.objects.all().order_by('created_on')
+
+        for post in posts:
+            existing_like = LikeModel.objects.filter(post_id=post.id, user=user)
+            if existing_like:
+                post.has_liked = True
+
+        return render(request, 'feed.html', {'posts': posts})
+    else:
+
+        return redirect('/login/')
+
+
+def like_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = LikeForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            existing_like = LikeModel.objects.filter(post_id=post_id, user=user).first()
+            if not existing_like:
+                LikeModel.objects.create(post_id=post_id, user=user)
+            else:
+                existing_like.delete()
+            return redirect('/feed/')
+    else:
+        return redirect('/login/')
+
+
+def comment_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            comment_text = form.cleaned_data.get('comment_text')
+            comment = CommentModel.objects.create(user=user, post_id=post_id, comment_text=comment_text)
+            comment.save()
+            return redirect('/feed/')
+        else:
+            return redirect('/feed/')
+    else:
+        return redirect('/login')
+
+
+# For validating the session
+def check_validation(request):
+    if request.COOKIES.get('session_token'):
+        session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
+        if session:
+            time_to_live = session.created_on + timedelta(days=1)
+            if time_to_live > timezone.now():
+                return session.user
+    else:
+        return None
